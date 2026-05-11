a468d2afa4dead99879afd19b67de3d2b173bcde
Mushfiqur Rahman Niaz <mushfiqurniaz33@gmail.com>
2026-05-11 07:12:12 +0600

    feat: add Docker development environment

--- scripts/entrypoint.dev.sh
+++ scripts/entrypoint.dev.sh
@@ -5,7 +5,8 @@ echo "🔧 Starting development entrypoint..."
 python scripts/wait_for_dependencies.py
 
 echo "📦 Running migrations..."
-python manage.py migrate --no-input
+python manage.py makemigrations
+python manage.py migrate
 
 echo "🚀 Starting Django dev server..."
 python manage.py runserver 0.0.0.0:8000
